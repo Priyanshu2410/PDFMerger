fb136cf5950a4dafca19392edab38678d0b99828
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-11-13 23:08:15 +0530

    some change

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const multer = require('multer')
+const multer  = require('multer')
+const {mergePdfs}  = require('/merge')
+
 const upload = multer({ dest: 'uploads/' })
+app.use('/static', express.static('public'))
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname , 'templets/index.html'))
+  res.sendFile(path.join(__dirname, "templates/index.html"))
 })
-app.post('/merge',upload.array('pdfs,2'), function(req, res,next)  {
+
+ 
+
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
   console.log(req.files)
-  res.send({data:req.files})
+  let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+  res.redirect(`http://localhost:3000/static/${d}.pdf` )
+
 })
 
 app.listen(port, () => {
